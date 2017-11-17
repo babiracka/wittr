06b13895155088441f4a16e86041e16f7e232799
Alina Babiracka <alina.babiracka@gmail.com>
2017-11-17 17:28:07 +0100

    3.13 solved

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,11 @@
 self.addEventListener('fetch', function(event) {
+  var url = event.request.url;
+  var def = url.substr(url.length - 4);
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  if(def === '.jpg') {
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
 });
