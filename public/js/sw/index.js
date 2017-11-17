b3b860973882d129bb4a5d7e837bbcf6c4e92fba
Alina Babiracka <alina.babiracka@gmail.com>
2017-11-17 17:34:13 +0100

    Task 3.11 ES6 update

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,6 +3,8 @@ self.addEventListener('fetch', function(event) {
   var def = url.substr(url.length - 4);
   // TODO: only respond to requests with a
   // url ending in ".jpg"
+
+  //ES6 alternative: if (event.request.url.endsWith('.jpg')) {}
   if(def === '.jpg') {
     event.respondWith(
       fetch('/imgs/dr-evil.gif')
